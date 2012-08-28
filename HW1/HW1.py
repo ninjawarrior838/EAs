37ef6b0eb173938799ca63a372c435823cbcc3a2
unknown <kyle.jamison42@gmail.com>
2012-08-28 15:26:55 -0700

    working file IO finally. Have local directorys working. Parsing almost working

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -3,26 +3,29 @@ import random
 import time
 
 
-def generateGraph():
-    return
-
-
 def main():
     if len(sys.argv) >= 2:  # check for the last comand line argument
         cfg = (sys.argv[-1])
     else:
         cfg = ('default.cfg')
     config = open(cfg, 'r')  # open config file
-    log = open(config.readline(), 'w')  # First line is the log file to use
-    answerFile = open(config.readline(), 'w')  # Second line is the answer file
-    dataFile = config.readline()  # Third line is the .dat file to use
+    log = open(config.readline().strip(), 'w')  # First line is the log file to use
+    answerFile = open(config.readline().strip(), 'w')  # Second line is the answer file
+    dataFile = config.readline().strip()  # Third line is the .dat file to use
     # check if a seed is provided, if 0, seed random from time in miliseconds
-    seedT = config.readline()
-    print seedT
-    if (seedT) == 0:
+    seedT = config.readline().strip()
+    if (seedT) == '0':
         seed = int(round(time.time() * 1000))
         random.seed(seed)
     else:
-        random.seed(seedT)
-    #for x in range(10):
-    print seed
+        random.seed(int(float(seedT)))
+    runs = int(float(config.readline().strip()))
+    evals = int(float(config.readline().strip()))
+
+
+def generateGraph():
+    return
+
+
+if __name__ == '__main__':
+    main()
